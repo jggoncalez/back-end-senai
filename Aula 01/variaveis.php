e2dc0f8ef156694b2ae7e486b3a4aedd4f06048b
jggoncalez <joaogabriel.racer@gmail.com>
2025-08-01 16:51:17 -0300

    AULA 01

---
+++ Aula 01/variaveis.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Olá! \n";
+
+    $nome = "João! \n";
+    $idade = "19";
+    $ano_atual = "2025";
+
+    $data_nasc = $ano_atual-$idade;
+    echo $nome, "você nasceu em: ", $data_nasc
+?>
