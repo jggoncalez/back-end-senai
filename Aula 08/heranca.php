aac6c901286c0d4c0eb94ef451920a7d2b6acd24
jggoncalez <joaogabriel.racer@gmail.com>
2025-08-29 16:49:50 -0300

    29.08.2025 - Aula 08

---
+++ Aula 08/heranca.php
@@ -0,0 +1,42 @@
+<?php
+
+class Imovel{
+    private $categoria;
+    private $n_comodos;
+    private $valor;
+    private $estado_conservacao;
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao){
+        $this->categoria = $categoria;
+        $this->n_comodos = $n_comodos;
+        $this->valor = $valor;
+        $this->estado_conservacao = $estado_conservacao;
+    }
+}
+
+class Casa extends Imovel{
+    private $tem_quintal; // Atributo booleano - true ou false
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao, $tem_quintal){
+        parent::__construct($categoria, $n_comodos, $valor, $estado_conservacao);
+        $this->tem_quintal = $tem_quintal;
+    }
+}
+
+class Apartamento extends Imovel{
+    private $andar;
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao, $andar){
+        parent::__construct($categoria, $n_comodos, $valor, $estado_conservacao);
+        $this->andar = $andar;
+    }
+}
+
+class Escola extends Imovel{
+    private $seguimento;
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao, $seguimento){
+        parent::__construct($categoria, $n_comodos, $valor, $estado_conservacao);
+        $this->seguimento = $seguimento;
+    }
+}
